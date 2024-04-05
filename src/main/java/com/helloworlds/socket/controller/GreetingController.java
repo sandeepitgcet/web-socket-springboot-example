1989de732651288d62851e7c9f2a13cc5b515c36
sandeep2197 <s.sandeep2197@gmail.com>
2024-04-05 15:23:34 +0530

    basic web socket connection

---
+++ src/main/java/com/helloworlds/socket/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.helloworlds.socket.controller;
+
+import com.helloworlds.socket.message.Greeting;
+import com.helloworlds.socket.message.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+}
